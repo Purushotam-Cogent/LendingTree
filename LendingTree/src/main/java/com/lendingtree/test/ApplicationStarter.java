4a5de2e3741ba4c8a45d808e152782e5cb771eef
purushotam.sinha@cogentinfo.com <purushotam.sinha@cogentinfo.com>
2018-10-19 03:13:48 +0530

    ---------------Lending Tree Week1 19-10-2018 ---------Github setup done-------------

---
+++ LendingTree/src/main/java/com/lendingtree/test/ApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.lendingtree.test;
+
+public class ApplicationStarter {
+
+	public static void main(String[] args) {
+		System.out.println("ApplicationStarter.main()");
+		System.out.println("Welcome to Lending Tree.........Apps");
+
+		
+
+	}
+
+}
