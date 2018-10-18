68ea99a97dc9d7fa9a73f7b13a2b5af974049b19
purushotam.sinha@cogentinfo.com <purushotam.sinha@cogentinfo.com>
2018-10-19 03:16:24 +0530

    --------------Modified the code-----------------

--- LendingTree/src/main/java/com/lendingtree/test/ApplicationStarter.java
+++ LendingTree/src/main/java/com/lendingtree/test/ApplicationStarter.java
@@ -5,9 +5,17 @@ public class ApplicationStarter {
 	public static void main(String[] args) {
 		System.out.println("ApplicationStarter.main()");
 		System.out.println("Welcome to Lending Tree.........Apps");
-
+		
+		System.out.println("User ID "+Integer.toHexString(new ApplicationStarter().getID()));
+	
 		
 
+	}
+	
+	
+	public int getID()
+	{
+		return this.hashCode();
 	}
 
 }
